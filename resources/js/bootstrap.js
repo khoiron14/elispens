48294c685419aa498a848c7395caafc61280bbe5
khoiron14 <khoironarkade@gmail.com>
2022-04-26 06:59:09 +0700

    using popperjs version 1

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,7 +2,7 @@ window._ = require('lodash');
 
 try {
     window.$ = window.jQuery = require('jquery');
-    window.Popper = require('@popperjs/core');
+    window.Popper = require('popper.js');
     window.bootstrap = require('bootstrap');
 } catch (e) {}
 
