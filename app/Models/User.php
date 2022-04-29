d366fb71fb67047dbcbcd7f9d49abb4f0c1c74f1
Tika Azahra <fajriahazahra@ggmail.com>
2022-04-29 17:02:12 +0700

    add model and migration course

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,4 +64,9 @@ class User extends Authenticatable
     {
         return $this->role === $role;
     }
+
+    public function course()
+    {
+        return $this->hasMany( related: Course::class);
+    }
 }
